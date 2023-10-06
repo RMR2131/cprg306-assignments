3b345804e80bf3dca2fbf8c5acb281fbce46343c
RMR2131 <ruth.reyes@edu.sait.ca>
2023-10-06 10:44:08 -0600

    Adding week4 Interactivity
    Add page.js and new-item.js

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Page() {
           <li><Link href="/week1">Week 1</Link></li>
           <li><Link href="/week2">Week 2</Link></li>
           <li><Link href="/week3">Week 3</Link></li>
+          <li><Link href="/week4">Week 4</Link></li>
         </ul>
       </div>
       </main>
